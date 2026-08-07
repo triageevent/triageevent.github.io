34ea7664adc00970e38382fbdb39e2b3da4c58d5
triageevent <vlodsindler@seznam.cz>
2026-08-07 16:38:58 +0200

    Implement changeMode function for theme switching
    Add changeMode function to switch between modes.

--- gamemode.js
+++ gamemode.js
@@ -1,1 +1,21 @@
+function changeMode(mode){
 
+if(mode=="night"){
+
+document.body.className="night";
+
+}
+
+if(mode=="sunrise"){
+
+document.body.className="sunrise";
+
+}
+
+if(mode=="dawn"){
+
+document.body.className="dawn";
+
+}
+
+}
