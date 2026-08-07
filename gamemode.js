ce708d649a10eaa7b6aca36264fdd37dadaf072f
triageevent <vlodsindler@seznam.cz>
2026-08-07 16:45:20 +0200

    Refactor changeMode function for clarity and structure

--- gamemode.js
+++ gamemode.js
@@ -1,21 +1,57 @@
 function changeMode(mode){
 
-if(mode=="night"){
 
-document.body.className="night";
+const body=document.body;
+
+const title=document.getElementById("mode-name");
+
+const text=document.getElementById("mode-text");
+
+
+
+if(mode==="night"){
+
+
+body.className="menu-page night";
+
+
+title.innerHTML="NIGHTFALL";
+
+text.innerHTML="Night operation phase";
+
 
 }
 
-if(mode=="sunrise"){
 
-document.body.className="sunrise";
+
+if(mode==="sunrise"){
+
+
+body.className="menu-page sunrise";
+
+
+title.innerHTML="SUNRISE";
+
+text.innerHTML="Day operation phase";
+
 
 }
 
-if(mode=="dawn"){
 
-document.body.className="dawn";
+
+if(mode==="dawn"){
+
+
+body.className="menu-page dawn";
+
+
+title.innerHTML="DAWN";
+
+text.innerHTML="Final operation phase";
+
 
 }
+
+
 
 }
